06d46d1ec556442afcd209aabd952afad4a983de
Adam Rehn <adam@adamrehn.com>
2022-04-21 15:26:34 +1000

    Add support for specifying a WebSocket URL when compiling the DOM delegate

--- delegates/dom/src/index.ts
+++ delegates/dom/src/index.ts
@@ -1,34 +1,40 @@
 import { NativeDOMDelegate } from "./NativeDOMDelegate";
 import * as libspsfrontend from '@tensorworks/libspsfrontend'
 
-// define our signallingServerProtocol to be used based on whether
-// or not we're accessing our frontend via a tls
-let signallingServerProtocol = 'ws:';
-if (location.protocol === 'https:') {
-    signallingServerProtocol = 'wss:';
+// Determine whether a signalling server WebSocket URL was specified at compile-time or if we need to compute it at runtime
+declare var WEBSOCKET_URL : string;
+let signallingServerAddress = WEBSOCKET_URL;
+if (signallingServerAddress == '')
+{
+    // define our signallingServerProtocol to be used based on whether
+    // or not we're accessing our frontend via a tls
+    let signallingServerProtocol = 'ws:';
+    if (location.protocol === 'https:') {
+        signallingServerProtocol = 'wss:';
+    }
+    
+    // build the websocket endpoint based on the protocol used to load the frontend
+    signallingServerAddress = signallingServerProtocol + '//' + window.location.hostname
+    
+    // if the frontend for an application is served from a base-level domain
+    // it has a trailing slash, so we need to account for this when appending the 'ws' for the websocket ingress
+    if(window.location.pathname == "/") {
+        signallingServerAddress += '/ws'
+    } else {
+        signallingServerAddress += (window.location.pathname + '/ws')
+    }
 }
 
-// build the websocket endpoint based on the protocol used to load the frontend
-let signallingServerAddress = signallingServerProtocol + '//' + window.location.hostname
-
-// if the frontend for an application is served from a base-level domain
-// it has a trailing slash, so we need to account for this when appending the 'ws' for the websocket ingress
-if(window.location.pathname == "/") {
-    signallingServerAddress += '/ws'
-} else {
-    signallingServerAddress += (window.location.pathname + '/ws')
-}
-
-// Create a config file
+// Create a config object
 let config = CreateConfig(signallingServerAddress, "player");
 
-// Create a Native DOM delegate instance that implements the Delegate interface class  
+// Create a Native DOM delegate instance that implements the Delegate interface class
 let delegate = new NativeDOMDelegate(config);
 
 // Create and return a new webRtcPlayerController instance 
 let RTCPlayer = create(config, delegate);
 
-// create takes in a delage interface type which our NativeDomDelegate class implements 
+// create takes in a delage interface type which our NativeDomDelegate class implements
 function create(config: libspsfrontend.Config, delegate: libspsfrontend.IDelegate) {
     return new libspsfrontend.webRtcPlayerController(config, delegate);
 }
--- delegates/dom/webpack.config.js
+++ delegates/dom/webpack.config.js
@@ -1,50 +1,58 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const webpack = require('webpack');
 
-module.exports = {
-  mode: 'development',
-  entry: {
-    index: './src/index.ts',
-    //index: './src/webRtcPlayer/webRtcPlayerController.ts',
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      title: 'Development',
-      template: './src/index.html',
-      filename: 'index.html'
-    }),
-    new MiniCssExtractPlugin()
-  ],
-  devtool: 'inline-source-map',
-  module: {
-      rules: [
-          {
-            test: /\.tsx?$/,
-            loader: 'ts-loader',
-            exclude: [
-              /node_modules/,
-            ],
-          },
-          {
-            test: /\.css$/i,
-            use: [MiniCssExtractPlugin.loader, 'css-loader'],
-          },
-      ],
-  },
-  resolve: {
-    extensions: ['.tsx', '.ts', '.js'],
-  },
-  output: {
-    filename: '[name].js',
-    //library: 'webRtcPlayerController',
-    library: 'frontend', // change this to something more meaningful
-    libraryTarget: 'umd',
-    path: path.resolve(__dirname, 'dist'),
-    clean: true,
-    globalObject: 'this'
-  },
-  optimization: {
-    minimize: false
-  },
-};
+module.exports = (env) => {
+  return {
+    mode: 'development',
+    entry: {
+      index: './src/index.ts',
+    },
+    plugins: [
+      
+      new webpack.DefinePlugin({
+        WEBSOCKET_URL: JSON.stringify((env.WEBSOCKET_URL !== undefined) ? env.WEBSOCKET_URL : '')
+      }),
+      
+      new HtmlWebpackPlugin({
+        title: 'Development',
+        template: './src/index.html',
+        filename: 'index.html'
+      }),
+      
+      new MiniCssExtractPlugin()
+      
+    ],
+    devtool: 'inline-source-map',
+    module: {
+        rules: [
+            {
+              test: /\.tsx?$/,
+              loader: 'ts-loader',
+              exclude: [
+                /node_modules/,
+              ],
+            },
+            {
+              test: /\.css$/i,
+              use: [MiniCssExtractPlugin.loader, 'css-loader'],
+            },
+        ],
+    },
+    resolve: {
+      extensions: ['.tsx', '.ts', '.js'],
+    },
+    output: {
+      filename: '[name].js',
+      library: 'frontend', // change this to something more meaningful
+      libraryTarget: 'umd',
+      path: path.resolve(__dirname, 'dist'),
+      clean: true,
+      globalObject: 'this'
+    },
+    optimization: {
+      minimize: false
+    },
+  };
+}
