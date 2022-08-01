9cc858aee11e4b7b000cfd1e65a22a47a1d7f4d1
Adrian Zahra <adrian@tensorworks.com.au>
2022-08-02 09:41:20 +1000

    reverted changes to auto play

--- library/src/Config/Config.ts
+++ library/src/Config/Config.ts
@@ -2,10 +2,10 @@ export class Config {
 
 	// PRESET OPTIONS
 	// enable the auto connect of the websocket 
-	enableSpsAutoConnect = true;
+	enableSpsAutoConnect = false;
 
 	// enable the autoplay of the video if enabled by browser
-	enableSpsAutoplay = true;
+	enableSpsAutoplay = false;
 
 	// start the video muted
 	startVideoMuted = false;
