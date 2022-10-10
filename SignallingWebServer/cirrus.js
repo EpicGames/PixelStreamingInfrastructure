a6d071a922794db55b06b8eab7ae227318f2e612
Luke Bermingham <luke.bermingham@xa.epicgames.com>
2022-10-10 15:56:03 +1000

    Bump version text to UE 5.1

--- SignallingWebServer/cirrus.js
+++ SignallingWebServer/cirrus.js
@@ -268,7 +268,7 @@ if (config.UseHTTPS) {
 	});
 }
 
-console.logColor(logging.Cyan, `Running Cirrus - The Pixel Streaming reference implementation signalling server for Unreal Engine 5.0.`);
+console.logColor(logging.Cyan, `Running Cirrus - The Pixel Streaming reference implementation signalling server for Unreal Engine 5.1.`);
 
 let nextPlayerId = 100; // reserve some player ids
 const SFUPlayerId = "1"; // sfu is a special kind of player
