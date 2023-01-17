345fdfc79faa6ce3bc413771be170fe7f0bfe5bb
Luke Bermingham <1215582+lukehb@users.noreply.github.com>
2023-01-17 10:17:29 +1000

    Cleaning up package.json files

--- implementations/EpicGames/index.d.ts
+++
@@ -1,3 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-declare module '*.png';
--- implementations/EpicGames/src/index.ts
+++ implementations/EpicGames/src/index.ts
@@ -2,21 +2,16 @@
 
 import * as libfrontend from '@epicgames/pixelstreamingfrontend';
 
-// Example of how to set the logger level
-//libfrontend.Logger.SetLoggerVerbosity(10);
 
-// Create a config object
-let config = new libfrontend.Config();
+document.body.onload = function() {
+	// Example of how to set the logger level
+	//libfrontend.Logger.SetLoggerVerbosity(10);
 
-// Create a Native DOM delegate instance that implements the Delegate interface class
-let application = new libfrontend.Application(config);
-// document.getElementById("centrebox").appendChild(application.rootElement);
-document.body.appendChild(application.rootElement);
+	// Create a config object
+	let config = new libfrontend.Config();
 
-// HACK: We want our body to be 100vh, but this doesn't work on iOS as the viewport height includes the address bar, leading to overflow and the need to scroll
-// To fix this we use the window innerHeight. This may not be necessary for your implementation
-const setBodyToWindowHeight = () => {
-	document.body.setAttribute("style", `background: #2e0052; width: 100vw; height: ${window.innerHeight}px`);
+	// Create a Native DOM delegate instance that implements the Delegate interface class
+	let application = new libfrontend.Application(config);
+	// document.getElementById("centrebox").appendChild(application.rootElement);
+	document.body.appendChild(application.rootElement);
 }
-window.addEventListener("resize", setBodyToWindowHeight);
-setBodyToWindowHeight();
