3870b53deddd1abb8ba7c653c3cd629ef8931451
Hannes Muurinen <hannes.muurinen@reaktor.com>
2023-02-13 15:43:52 +0200

    a sample UI application with no overlay UI

---
+++ Frontend/implementations/EpicGames/src/uiless.ts
@@ -0,0 +1,14 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+import * as libfrontend from '@epicgames-ps/lib-pixelstreamingfrontend-dev';
+
+document.body.onload = function() {
+	// Example of how to set the logger level
+	//libfrontend.Logger.SetLoggerVerbosity(10);
+
+	// Create a config object
+	let config = new libfrontend.Config();
+
+	// Create a PixelStreaming instance and attach the video element to an existing parent div
+	let pixelStreaming = new libfrontend.PixelStreaming(config, { videoElementParent: document.getElementById("videoParentElement")});
+}
