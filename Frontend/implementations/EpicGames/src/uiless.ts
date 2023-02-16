eb8e2e9e145b36a897429120da467020ac88e9e5
Hannes Muurinen <hannes.muurinen@reaktor.com>
2023-02-16 09:25:14 +0200

    configure initial params in uiless.ts to auto start muted

--- Frontend/implementations/EpicGames/src/uiless.ts
+++ Frontend/implementations/EpicGames/src/uiless.ts
@@ -7,7 +7,12 @@ document.body.onload = function() {
 	// Logger.SetLoggerVerbosity(10);
 
 	// Create a config object
-	const config = new Config();
+	const config = new Config({
+		AutoPlayVideo: true,
+		AutoConnect: true,
+		ss: "ws://localhost:80",
+		StartVideoMuted: true,
+	});
 
 	// Create a PixelStreaming instance and attach the video element to an existing parent div
 	const pixelStreaming = new PixelStreaming(config, { videoElementParent: document.getElementById("videoParentElement")});
