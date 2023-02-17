b9dea21f6bf2526d7510f9d4d126e81c0c6a83f0
Luke Bermingham <1215582+lukehb@users.noreply.github.com>
2023-02-17 17:17:22 +1000

    Added dev and prod configs to webpack (#102)

---
+++ Frontend/implementations/EpicGames/webpack.dev.js
@@ -0,0 +1,10 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+const { merge } = require('webpack-merge');
+const common = require('./webpack.common.js');
+const path = require('path');
+
+module.exports = merge(common, {
+    mode: 'development',
+    devtool: 'inline-source-map',
+});
