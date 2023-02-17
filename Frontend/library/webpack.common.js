92dd46dfc1798e94fdeaeeb7c31ab5a7d4600fe3
Luke Bermingham <1215582+lukehb@users.noreply.github.com>
2023-02-17 18:03:37 +1000

    Bring Github actions to 5.2 branch (#105)
    * Added dev and prod configs to webpack (#102)
    * Adding github actions to create an NPM package for frontend library and make a release for the repo (#103)
    * Added workflow file for pushing library to npm
    * Added workflow file for making a release with built implementation/EpicGames archives

--- Frontend/library/webpack.common.js
+++ Frontend/library/webpack.common.js
@@ -5,11 +5,9 @@ const path = require('path');
 const webpack = require('webpack');
 
 module.exports = {
-    mode: 'development',
     entry: {
         index: './src/pixelstreamingfrontend.ts'
     },
-    devtool: 'inline-source-map',
     module: {
         rules: [
             {
@@ -28,15 +26,10 @@ module.exports = {
         })
     ],
     output: {
-        filename: 'lib-pixelstreamingfrontend.min.js',
         library: 'lib-pixelstreamingfrontend', // exposed variable that will provide access to the library classes
         libraryTarget: 'umd',
         path: path.resolve(__dirname, 'dist'),
-        clean: false,
+        clean: true,
         globalObject: 'this'
-    },
-    optimization: {
-        minimize: false
-    },
-    stats: 'errors-only'
+    }
 };
