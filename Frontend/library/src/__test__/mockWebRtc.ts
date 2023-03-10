63f664b89b41d8edbf1593dd03fdb6686f5499d8
Hannes Muurinen <hannes.muurinen@reaktor.com>
2023-03-10 16:47:51 +0200

    mock video codecs list

--- Frontend/library/src/__test__/mockWebRtc.ts
+++ Frontend/library/src/__test__/mockWebRtc.ts
@@ -1,7 +1,13 @@
 export const MockRTCRtpReceiver = {
     prototype: jest.fn(),
     getCapabilities: () => ({
-        codecs: [] as RTCRtpCodecCapability[],
+        codecs: [
+            {
+                clockRate: 60,
+                mimeType: "testMimeType",
+                sdpFmtpLine: "AV1"
+            }
+        ] as RTCRtpCodecCapability[],
         headerExtensions: [] as RTCRtpHeaderExtensionCapability[]
     })
 } as any as typeof global.RTCRtpReceiver;
