20018776d27b686276b7e2b0992fc619f35fa7ed
Hannes Muurinen <hannes.muurinen@reaktor.com>
2023-03-10 17:33:22 +0200

    test for PixelStreaming SettingsChangedEvent

---
+++ Frontend/library/src/PixelStreaming/PixelStreaming.test.ts
@@ -0,0 +1,42 @@
+import { MockRTCRtpReceiver } from '../__test__/mockWebRtc';
+import {
+    Config,
+    Flags,
+    FlagsKeys,
+    NumericParameters,
+    NumericParametersKeys,
+    OptionParameters,
+    OptionParametersKeys,
+    TextParameters,
+    TextParametersKeys
+} from '../Config/Config';
+import { PixelStreaming } from './PixelStreaming';
+import { SettingsChangedEvent } from '../pixelstreamingfrontend';
+
+describe('PixelStreaming', () => {
+    beforeEach(() => {
+        global.RTCRtpReceiver = MockRTCRtpReceiver;
+    });
+
+    afterEach(() => {
+        jest.resetAllMocks();
+    });
+
+    it('should emit settingsChanged events when the configuration is updated', () => {
+        const config = new Config();
+        const pixelStreaming = new PixelStreaming(config);
+
+        const settingsChangedSpy = jest.fn();
+        pixelStreaming.addEventListener("settingsChanged", settingsChangedSpy);
+
+        expect(settingsChangedSpy).not.toHaveBeenCalled();
+        config.setNumericSetting(NumericParameters.WebRTCMaxBitrate, 123);
+        expect(settingsChangedSpy).toHaveBeenCalledWith(new SettingsChangedEvent({
+            id: NumericParameters.WebRTCMaxBitrate,
+            target: config.getNumericSettings().find((setting) => setting.id === NumericParameters.WebRTCMaxBitrate)!,
+            type: 'number',
+            value: 123,
+        }));
+    });
+
+});
