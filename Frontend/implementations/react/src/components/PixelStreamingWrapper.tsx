b7f1643ea943529962b7b6a85a485bb4380d5785
Hannes Muurinen <hannes.muurinen@reaktor.com>
2023-03-17 10:41:55 +0200

    added step-by-step comments explaining how PixelStreamingWrapper works

--- Frontend/implementations/react/src/components/PixelStreamingWrapper.tsx
+++ Frontend/implementations/react/src/components/PixelStreamingWrapper.tsx
@@ -14,22 +14,33 @@ export interface PixelStreamingWrapperProps {
 export const PixelStreamingWrapper = ({
     initialSettings
 }: PixelStreamingWrapperProps) => {
+    // A reference to parent div element that the Pixel Streaming library attaches into:
     const videoParent = useRef<HTMLDivElement>(null);
 
+    // Pixel streaming library instance is stored into this state variable after initialization:
     const [pixelStreaming, setPixelStreaming] = useState<PixelStreaming>();
+    
+    // A boolean state variable that determines if the Click to play overlay is shown:
     const [clickToPlayVisible, setClickToPlayVisible] = useState(false);
 
+    // Run on component mount:
     useEffect(() => {
         if (videoParent.current) {
+            // Attach Pixel Streaming library to videoParent element:
             const config = new Config({ initialSettings });
             const streaming = new PixelStreaming(config, {
                 videoElementParent: videoParent.current
             });
+            
+            // register a playStreamRejected handler to show Click to play overlay if needed:
             streaming.addEventListener('playStreamRejected', () => {
                 setClickToPlayVisible(true);
             });
 
+            // Save the library instance into component state so that it can be accessed later:
             setPixelStreaming(streaming);
+
+            // Clean up on component unmount:
             return () => {
                 try {
                     streaming.disconnect();
