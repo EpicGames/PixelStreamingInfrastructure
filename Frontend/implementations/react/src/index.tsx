c66e80ae7d64551dc0fac07d68718f96e73eb496
Hannes Muurinen <hannes.muurinen@reaktor.com>
2023-03-17 10:09:09 +0200

    prettier formatting

--- Frontend/implementations/react/src/index.tsx
+++ Frontend/implementations/react/src/index.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { App } from './components/App';
 
-document.body.onload = function() {
-	// Attach the React app root component to document.body
-	createRoot(document.body).render(<App />);
-}
+document.body.onload = function () {
+    // Attach the React app root component to document.body
+    createRoot(document.body).render(<App />);
+};
