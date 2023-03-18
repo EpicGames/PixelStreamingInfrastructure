efd1fbc3ff0ab4f39a0416f87c349d21eabbe14f
hmuurine <hmuurine@users.noreply.github.com>
2023-03-18 03:04:50 +0200

    Minimal sample React implementation (#159)
    * Minimal sample React implementation
    
    * Added step-by-step comments explaining how PixelStreamingWrapper works

---
+++ Frontend/implementations/react/src/components/App.tsx
@@ -0,0 +1,25 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+import React from 'react';
+import { PixelStreamingWrapper } from './PixelStreamingWrapper';
+
+export const App = () => {
+    return (
+        <div
+            style={{
+                height: '100%',
+                width: '100%'
+            }}
+        >
+            <PixelStreamingWrapper
+                initialSettings={{
+                    AutoPlayVideo: true,
+                    AutoConnect: true,
+                    ss: 'ws://localhost:80',
+                    StartVideoMuted: true,
+                    HoveringMouse: true
+                }}
+            />
+        </div>
+    );
+};
