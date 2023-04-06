b3a3304bcb1bded1e6f2abf70574e20f85bdccb6
Matthew Cotton <matt@tensorworks.com.au>
2023-04-06 11:47:04 +1000

    i hate docker

--- SignallingWebServer/platform_scripts/bash/docker-build-cirrus.sh
+++ SignallingWebServer/platform_scripts/bash/docker-build-cirrus.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 # Copyright Epic Games, Inc. All Rights Reserved.
 
+cp ../../../turnserver.conf .
+
 # When run from SignallingWebServer/platform_scripts/bash, this uses the SignallingWebServer directory
 # as the build context so the Cirrus files can be successfully copied into the container image
 docker build --network=host -t 'cirrus-webserver:latest' -f ./Dockerfile ../..
