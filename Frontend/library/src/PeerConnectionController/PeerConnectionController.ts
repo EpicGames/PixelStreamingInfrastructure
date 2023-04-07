89d8505181039fc0d2e8c8203f1cb0fadfa85d66
k_a_s_s <15650245+kass-kass@users.noreply.github.com>
2023-04-07 13:25:02 +0200

    Added option to (un)mute mic in PixelStreaming

--- Frontend/library/src/PeerConnectionController/PeerConnectionController.ts
+++ Frontend/library/src/PeerConnectionController/PeerConnectionController.ts
@@ -4,6 +4,7 @@ import { Logger } from '../Logger/Logger';
 import { Config, OptionParameters, Flags } from '../Config/Config';
 import { AggregatedStats } from './AggregatedStats';
 import { parseRtpParameters, splitSections } from 'sdp';
+import { RTCUtils } from '../Util/RTCUtils';
 
 /**
  * Handles the Peer Connection
@@ -371,12 +372,7 @@ export class PeerConnectionController {
         // We can only set preferrec codec on Chrome
         if (RTCRtpReceiver.getCapabilities && this.preferredCodec != '') {
             for (const transceiver of this.peerConnection?.getTransceivers() ?? []) {
-                if (
-                    transceiver &&
-                    transceiver.receiver &&
-                    transceiver.receiver.track &&
-                    transceiver.receiver.track.kind === 'video'
-                ) {
+                if (RTCUtils.canTransceiverReceiveVideo(transceiver)) {
                     const preferredRTPCodec = this.preferredCodec.split(' ');
                     const codecs = [
                         {
@@ -426,18 +422,16 @@ export class PeerConnectionController {
             });
         } else {
             // set the audio options based on mic usage
-            const audioOptions = useMic
-                ? {
-                      autoGainControl: false,
-                      channelCount: 1,
-                      echoCancellation: false,
-                      latency: 0,
-                      noiseSuppression: false,
-                      sampleRate: 48000,
-                      sampleSize: 16,
-                      volume: 1.0
-                  }
-                : false;
+            const audioOptions = {
+                autoGainControl: false,
+                channelCount: 1,
+                echoCancellation: false,
+                latency: 0,
+                noiseSuppression: false,
+                sampleRate: 48000,
+                sampleSize: 16,
+                volume: 1.0
+            }
 
             // set the media send options
             const mediaSendOptions: MediaStreamConstraints = {
@@ -452,12 +446,7 @@ export class PeerConnectionController {
             if (stream) {
                 if (hasTransceivers) {
                     for (const transceiver of this.peerConnection?.getTransceivers() ?? []) {
-                        if (
-                            transceiver &&
-                            transceiver.receiver &&
-                            transceiver.receiver.track &&
-                            transceiver.receiver.track.kind === 'audio'
-                        ) {
+                        if (RTCUtils.canTransceiverReceiveAudio(transceiver)) {
                             for (const track of stream.getTracks()) {
                                 if (track.kind && track.kind == 'audio') {
                                     transceiver.sender.replaceTrack(track);
