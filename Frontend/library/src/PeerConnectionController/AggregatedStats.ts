5a69ed20d66db3feffc4b519304f97e8da9ee448
Chris Hassebrook <chasse20@gmail.com>
2023-05-17 15:50:37 -0400

    Temporary hack until Typescript updates definition for RTCStatsType
    Signed-off-by: Chris Hassebrook <chasse20@gmail.com>

--- Frontend/library/src/PeerConnectionController/AggregatedStats.ts
+++ Frontend/library/src/PeerConnectionController/AggregatedStats.ts
@@ -19,7 +19,7 @@ import { Logger } from '../Logger/Logger';
  * The Aggregated Stats that is generated from the RTC Stats Report
  */
 
-type RTCStatsTypePS = RTCStatsType | 'stream';
+type RTCStatsTypePS = RTCStatsType | 'stream' | 'media-playout';
 export class AggregatedStats {
     inboundVideoStats: InboundVideoStats;
     inboundAudioStats: InboundAudioStats;
