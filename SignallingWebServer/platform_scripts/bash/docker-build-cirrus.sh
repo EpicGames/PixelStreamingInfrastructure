e746d599c96c0cd5ef260131d8a01f1985151e1f
William Belcher <william.belcher@xa.epicgames.com>
2023-07-27 15:35:31 +1000

    Update build context location

--- SignallingWebServer/platform_scripts/bash/docker-build-cirrus.sh
+++ SignallingWebServer/platform_scripts/bash/docker-build-cirrus.sh
@@ -6,5 +6,5 @@ pushd "${BASH_LOCATION}" > /dev/null
 
 # When run from SignallingWebServer/platform_scripts/bash, this uses the SignallingWebServer directory
 # as the build context so the Cirrus files can be successfully copied into the container image
-docker build --network=host -t 'cirrus-webserver:latest' "${BASH_LOCATION}/../../Dockerfile"
+docker build --network=host -t 'cirrus-webserver:latest' -f "${BASH_LOCATION}/../../Dockerfile" "${BASH_LOCATION}/../../.."
 
