df3bcb9f838c1a481e7d84dbd4e7c0a7545451bd
Matthew Cotton <matt@tensorworks.com.au>
2024-01-17 13:16:43 +1100

    very basic message validation. isnt going to work since required/optional arent reflected in the generated code.

--- SS_Test/src/message_helpers.ts
+++ SS_Test/src/message_helpers.ts
@@ -17,12 +17,24 @@ export function getProtoMessageFromString<T extends BaseMessage>(jsonString: str
 	return getProtoMessage(parsed);
 }
 
+function haveSameFields(obj1: any, obj2: any): boolean {
+  const keys1 = Object.keys(obj1).filter(key => key !== "$type").sort();
+  const keys2 = Object.keys(obj2).filter(key => key !== "$type").sort();
+  return keys1.length === keys2.length && keys1.every((key, index) => key === keys2[index]);
+}
+
 export function getProtoMessage<T extends BaseMessage>(msg: any): T | null {
 	let messageType = messageTypeRegistry.get(msg.type);
 	if (!messageType) {
 		return null;
 	}
-	return messageType.fromJSON(msg) as T;
+	let parsed = messageType.fromJSON(msg) as T;
+	let empty = messageType.fromPartial({});
+	if (!haveSameFields(empty, msg)) {
+		console.log(`bad incoming message?`);
+		console.log(`${JSON.stringify(msg)}`);
+	}
+	return parsed;
 }
 
 export function protoToJSON(msg: any): unknown {
